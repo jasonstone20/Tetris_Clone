aff90f0f79519c3d640d394d76517f0a4efb1671
Jason D. Stone <jasonstone20@yahoo.com>
2020-03-06 11:41:17 -0800

    Add files via upload

--- tetris.cpp
+++ tetris.cpp
@@ -15,203 +15,196 @@ class Tetrominos
 {
 public:
 
-	Tetrominos();
-	~Tetrominos();
+//	Tetrominos();
+//	~Tetrominos();
 
 	// Line Tetromino
-	int tetrominoLine1[4] ={00100,
+	int tetrominoLine1[5] ={00100,
 							00100,
 							00100,
 							00100,
 							00000};
 
-	int tetrominoLine2[4] ={00000,
+	int tetrominoLine2[5] ={00000,
 							00000,
 						    01111,
 						    00000,
 							00000};
 	
-	int tetrominoLine3[4] ={00000,
+	int tetrominoLine3[5] ={00100,
 							00100,
 							00100,
 							00100,
-							00100};
+							00000};
 
-	int tetrominoLine4[4] ={00000,
+	int tetrominoLine4[5] ={00000,
 							00000,
-							11110,
+							01111,
 							00000,
 							00000};
 							
 	// Square Tetromino
 	
-	int tetrominoSqar1[4] ={00000,
+	int tetrominoSqar1[5] ={00000,
                             00000,
                             00110,
                             00110,
                             00000};
 
-    int tetrominoSqar2[4] ={00000,
+    int tetrominoSqar2[5] ={00000,
                             00000,
                             00110,
                             00110,
                             00000};
     
-    int tetrominoSqar3[4] ={00000,
+    int tetrominoSqar3[5] ={00000,
                             00000,
                             00110,
                             00110,
                             00000};
 
-    int tetrominoSqar4[4] ={00000,
+    int tetrominoSqar4[5] ={00000,
                             00000,
                             00110,
                             00110,
                             00000};
 	// Z Tetromino					
 	
-	int tetrominoLine1[4] ={00100,
-                            00100,
-                            00100,
-                            00100,
+	int tetrominoZTet1[5] ={00000,
+                            00000,
+                            01100,
+                            00110,
                             00000};
 
-    int tetrominoLine2[4] ={00000,
-                            00000,
-                            01111,
-                            00000,
+    int tetrominoZTet2[5] ={00000,
+                            00100,
+                            01100,
+                            01000,
                             00000};
 
-    int tetrominoLine3[4] ={00000,
-                            00100,
-                            00100,
-                            00100,
-                            00100};
+    int tetrominoZTet3[5] ={00000,
+                            00000,
+                            01100,
+                            00110,
+                            00000};
 
-    int tetrominoLine4[4] ={00000,
-                            00000,
-                            11110,
-                            00000,
+    int tetrominoZTet4[5] ={00000,
+                            00100,
+                            01100,
+                            01000,
                             00000};
 	// S Tetromino					
 	
-	int tetrominoLine1[4] ={00100,
-                            00100,
-                            00100,
+	int tetrominoSTet1[5] ={00000,
+                            01000,
+                            01100,
                             00100,
                             00000};
 
-    int tetrominoLine2[4] ={00000,
-                            00000,
-                            01111,
+    int tetrominoSTet2[5] ={00000,
+                            00110,
+                            01100,
                             00000,
                             00000};
 
-    int tetrominoLine3[4] ={00000,
+    int tetrominoSTet3[5] ={00000,
                             00100,
-                            00100,
-                            00100,
-                            00100};
+                            00110,
+                            00010,
+                            00000};
 
-    int tetrominoLine4[4] ={00000,
-                            00000,
-                            11110,
+    int tetrominoSTet4[5] ={00000,
+                            00110,
+                            01100,
                             00000,
                             00000};
 	// T Tetromino
 	
-	int tetrominoLine1[4] ={00100,
+	int tetrominoTTet1[5] ={00000,
                             00100,
-                            00100,
-                            00100,
-                            00000};
-
-    int tetrominoLine2[4] ={00000,
-                            00000,
-                            01111,
+                            01110,
                             00000,
                             00000};
 
-    int tetrominoLine3[4] ={00000,
+    int tetrominoTTet2[5] ={00000,
                             00100,
+                            00110,
                             00100,
-                            00100,
-                            00100};
+                            00000};
 
-    int tetrominoLine4[4] ={00000,
+    int tetrominoTTet3[5] ={00000,
                             00000,
-                            11110,
-                            00000,
+                            01110,
+                            00100,
+                            00000};
+
+    int tetrominoTTet4[5] ={00000,
+                            00100,
+                            01100,
+                            00100,
                             00000};
 	// L Tetromino				
 	
-	int tetrominoLine1[4] ={00100,
+	int tetrominoLTet1[5] ={00000,
                             00100,
+                            00100,
+                            00110,
+                            00000};
+
+    int tetrominoLTet2[5] ={00000,
+                            00000,
+                            01110,
+                            01000,
+                            00000};
+
+    int tetrominoLTet3[5] ={00000,
+                            01100,
                             00100,
                             00100,
                             00000};
 
-    int tetrominoLine2[4] ={00000,
-                            00000,
-                            01111,
-                            00000,
-                            00000};
-
-    int tetrominoLine3[4] ={00000,
-                            00100,
-                            00100,
-                            00100,
-                            00100};
-
-    int tetrominoLine4[4] ={00000,
-                            00000,
-                            11110,
+    int tetrominoLTet4[5] ={00000,
+                            00010,
+                            01110,
                             00000,
                             00000};
 	// Reverse L Tetromino
 	
-	int tetrominoLine1[4] ={00100,
+	int tetromino7Tet1[5] ={00000,
                             00100,
                             00100,
-                            00100,
+                            01100,
                             00000};
 
-    int tetrominoLine2[4] ={00000,
-                            00000,
-                            01111,
+    int tetromino7Tet2[5] ={00000,
+                            01000,
+                            01110,
                             00000,
                             00000};
     
-    int tetrominoLine3[4] ={00000,
+    int tetromino7Tet3[5] ={00000,
+                            00110,
                             00100,
                             00100,
-                            00100,
-                            00100};
+                            00000};
 
-    int tetrominoLine4[4] ={00000,
+    int tetromino7Tet4[5] ={00000,
                             00000,
-                            11110,
-                            00000,
+                            01110,
+                            00010,
                             00000};
 		
 					
 };
 
-Tetrominos::Tetrominos(int tet)
-{
-	for(int i=0;i<15;i++)
-		{
-			tetromino[tet]	
-		}
-}
-
-void piece1()
-{
-	string tet1[]
-	tet1[15] = "..X." 
-			..X.,
-			..X.,
-}
+//Tetrominos::Tetrominos(int tet)
+//{
+//	for(int i=0;i<4;i++)
+//		{
+//			tetromino[tet]	
+//		}
+//}
+int
 
 //Game Movement
 //
@@ -241,7 +234,6 @@ int GameLoop()
 
 int main(int argc, char **argv)
 {
-	printw("Tetrominos %c", tet[15]);
 	GameLoop();
 
 };
